000fb8c94a32287eeae3e3acfe3e7fde32d4f237
Dmitriy937 <dima57986@gmail.com>
2023-05-04 14:51:52 +0300

    hw4

---
+++ HW-4.js
@@ -0,0 +1,50 @@
+//1 задание
+/*let a = 0;
+
+while (a < 2) {
+    console.log('Привет');
+    a++;
+}*/
+
+//2 задание
+/*let b = 1;
+
+while (b < 6) {
+    console.log(b);
+    b++;
+}*/
+
+//3 задание
+/*let c = 7;
+
+while (c < 23) {
+    console.log(c);
+    c++;
+}*/
+
+//4 задание
+/*const obj = {
+    Коля: '200',
+    Вася: '300',
+    Петя: '400',
+}
+
+for (const key in obj) {
+    alert(key + ' - зарплата ' + obj[key] + ' долларов' );
+}*/
+
+//5 задание
+/*let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+    num++;
+    n /= 2;
+    console.log(num);
+    console.log(n);
+}*/
+
+//6 задание
+for (let dayFriday = 5; dayFriday <= 31; dayFriday += 7) {
+    console.log(`Сегодня пятница, ${dayFriday}-е число. Необходимо подготовить отчет.`);
+}
