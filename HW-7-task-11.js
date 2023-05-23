04d8766b97a1a9eac04293291fd165e76df1fab0
Dmitriy937 <dima57986@gmail.com>
2023-05-23 23:19:31 +0300

    hw-7-modified

--- HW-7-task-11.js
+++ HW-7-task-11.js
@@ -1,7 +1,6 @@
-let month = Number(prompt('Введите номер месяца от 1 до 12'));
-let season = ['Зима', 'Весна', 'Лето', 'Осень'];
-
 function monthOfYear() {
+    let month = Number(prompt('Введите номер месяца от 1 до 12'));
+    let season = ['Зима', 'Весна', 'Лето', 'Осень'];
     if (month <= 12 || month <= 1) {
         return 'Время года: ' + season[Math.floor(month/3)];
     } else if (Number.isNaN(month)) {
@@ -10,8 +9,6 @@ function monthOfYear() {
         return 'В году всего 12 месяцев, повтори попытку))';
     }
 }
-
-alert(monthOfYear());
 
 //task 7.11
 function shuffle() {
