a1c4c983c2ccef464d3c73032590b883bb34ae80
Dmitriy937 <dima57986@gmail.com>
2023-05-24 19:06:32 +0300

    hw7-modified v.3

--- HW-7-task-11.js
+++ HW-7-task-11.js
@@ -2,24 +2,24 @@ function monthOfYear() {
     let month = Number(prompt('Введите номер месяца от 1 до 12'));
     let season = ['Зима', 'Весна', 'Лето', 'Осень'];
     if (month <= 12 || month <= 1) {
-        return 'Время года: ' + season[Math.floor(month/3)];
+        return alert('Время года: ' + season[Math.floor(month/3)]);
     } else if (Number.isNaN(month)) {
-        return 'Вы ввели неверное значение';
+        return alert('Вы ввели неверное значение');
     } else {
-        return 'В году всего 12 месяцев, повтори попытку))';
+        return alert('В году всего 12 месяцев, повтори попытку))');
     }
 }
 
 //task 7.11
 function shuffle() {
-    const fruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    let fruit = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
     fruit = fruit.sort(() => Math.random() - 0.5);
     alert(fruit);
     let answer = prompt('Чему равнялся первый элемент массива?');
     let answer2 = prompt('Чему равнялся последний элемент массива?');
     if (answer.toLowerCase() == fruit[0].toLowerCase() && answer2.toLowerCase() == fruit[6].toLowerCase()) {
         alert('Поздравляем Вы угадали оба элемента');
-    } else if(answer.toLowerCase() == fruit[0].toLowerCase || answer2.toLowerCase() == fruit[6].toLowerCase()) {
+    } else if(answer.toLowerCase() == fruit[0].toLowerCase() || answer2.toLowerCase() == fruit[6].toLowerCase()) {
         alert('Вы почти угадали, Вы были близки к победе!');
     } else {
         alert('Вы ответили неверно');
